97b308d0855bb4564717a35f21782278ad5ae5cf
Angie Soto <angie1.soto@ucp.edu.co>
2021-07-20 22:11:35 -0500

    desarrollo componente factura y funcionalidades

--- taller4/src/app/factura/factura.component.ts
+++ taller4/src/app/factura/factura.component.ts
@@ -1,3 +1,4 @@
+
 import { CATCH_ERROR_VAR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { Factura } from './factura.model';
@@ -10,12 +11,113 @@ import {NgbModal, ModalDismissReasons, NgbModule} from '@ng-bootstrap/ng-bootstr
   styleUrls: ['./factura.component.css']
 })
 export class FacturaComponent implements OnInit {
-
+  items: Items[] = [];
+  facturas: Factura[] = [];
+  nombre = "";
+  precio=0;
+  idFactura=0;
+  cliente="";
+  fecha = "";
+  id=20210;
+  idFact=0;
+  sinFactura:boolean=true;
+  desabilitadoBtnFact=true;
+  desabilitadoBtnItem=true;
  
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
+ ////-----------funviones que validan los formularios-------------------
+  validarFormFact(){
+    try{
+      
+      if(this.fecha!="" && this.cliente!=""){
+        this.desabilitadoBtnFact=false
+      }
+      else{
+        this.desabilitadoBtnFact=true
+      }
 
+     
+    }
+    catch(e){
+      console.log("An error occurred on validarFormFact=>",e)
+    }
+  }
+
+  validarFormItem(){
+    try{
+      
+      if(this.nombre!="" && this.precio!=0){
+     
+        this.desabilitadoBtnItem=false
+      }
+      else{
+        this.desabilitadoBtnItem=true
+      }
+    
+     
+    }
+    catch(e){
+      console.log("An error occurred on validarFormItem=>",e)
+    }
+  }
+
+  ///--------funciones que abren los popup---------------------------
+  openAgregarItem(idF: number,content: any){
+    try{
+      
+      this.idFact=idF;
+      console.log(idF)
+      this.desabilitadoBtnItem=true;
+      this.modalService.open(content)
+     
+    }
+    catch(e){
+      console.log("An error occurred on openAgregarItem=>",e)
+    }
+  }
+  
+
+///----------funcion agregar items a un factura------------------------
+  agregarItem(idFact:number){
+    try{
+      let item = new Items(this.nombre,this.precio,idFact)
+      var buscarFactura=this.facturas.find(facturita=>facturita.id==idFact)
+      if(buscarFactura==undefined){
+        alert("el id de factura no existe")
+      }
+      else{
+        buscarFactura.items.push(item)
+        this.nombre="";
+        this.precio=0;
+        this.desabilitadoBtnItem=true;
+        
+      }
+     console.log(this.facturas)
+    }
+    catch(e){
+      console.log("An error occurred on agregarItem=>",e)
+    }
+  }
+
+///----------funcion agrega una factura al sistema---------------------
+  crearFactura(){
+    try{
+      let factura = new Factura(this.fecha,this.cliente,this.id,[])
+      this.facturas.push(factura);
+      this.id=this.id+1;
+      this.sinFactura=false;
+      this.fecha="";
+      this.cliente="";
+      this.desabilitadoBtnFact=true
+      
+    }
+    catch(e){
+      console.log("An error occurred on crearFactura=>",e)
+    }
+    
+  }
 
 }
